57b34aa0060f6c3527b8c35103f0cc591fee1ce7
Silver Wei <silveracgal@gmail.com>
2016-01-05 13:44:39 +0800

    Initial Commit

---
+++ YiZe_DigitalMallTests/YiZe_DigitalMallTests.swift
@@ -0,0 +1,36 @@
+//
+//  YiZe_DigitalMallTests.swift
+//  YiZe_DigitalMallTests
+//
+//  Created by dmqlMAC on 16/1/5.
+//  Copyright © 2016年 dmqlMAC. All rights reserved.
+//
+
+import XCTest
+@testable import YiZe_DigitalMall
+
+class YiZe_DigitalMallTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
